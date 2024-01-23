660143c88c481d8f33434950add1370c19fb7c72
Yuhtin <rio.daviduarte@gmail.com>
2024-01-23 02:27:47 -0300

    feat: exercise 1

---
+++ exercise_01.js
@@ -0,0 +1,12 @@
+var sum = (number1, number2) => number1 + number2
+var subtract = (number1, number2) => number1 - number2
+var multiply = (number1, number2) => number1 * number2
+var divide = (number1, number2) => number1 / number2
+
+var number1 = 10
+var number2 = 5
+
+console.log("Sum: " + sum(10, 5))
+console.log("Subtract: " + subtract(10, 5))
+console.log("Multiply: " + multiply(10, 5))
+console.log("Divide: " + divide(10, 5))
